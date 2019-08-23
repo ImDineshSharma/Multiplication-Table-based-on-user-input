e79e861b3bf680130da0b6d8c8f5ef822a738f9d
Dinesh Sharma <39302151+MeetDineshSharma@users.noreply.github.com>
2019-08-23 16:14:49 +0530

    Create Program.java

---
+++ Program.java
@@ -0,0 +1,16 @@
+// Enter any value(int) and it will print the Multiplication table for that value
+
+import java.util.Scanner
+public class Table{
+  public static void main(String[] args){
+    
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter any numarical value");
+    int input = sc.nextInt();
+    
+    for(int counter = 1; counter<=10; counter++){
+      System.out.println( input + "x" + counter +  " = "input*counter);
+    }
+    
+  }
+}
